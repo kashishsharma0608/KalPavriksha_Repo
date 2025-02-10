b15e727e807c17edd7ef4b5b1c95004646ebaac5
Kashish sharma <161445383+kashishsharma0608@users.noreply.github.com>
2025-02-10 10:50:52 +0530

    Fix: removed Comments and reduce redundancy from code

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,144 +1,124 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node
-{
+
+typedef struct node {
     int id;
     int power;
     struct node *next;
 } node;
-void createLinkedList(node **start, int id, int powerValue)
-{
-    node *newnNode = (node *)malloc(sizeof(node));
-    newnNode->id = id;
-    newnNode->power = powerValue;
-    newnNode->next = NULL;
-    if (*start == NULL)
-    {
-        *start = newnNode;
-    }
-    else
-    {
-        node *ptr = *start;
-        while (ptr->next != NULL)
-        {
-            ptr = ptr->next;
-        }
-        ptr->next = newnNode;
-    }
-}
-void display(node *start)
-{
-    if (start == NULL)
-    {
-        printf("List is empty");
+
+void createLinkedList(node **start, int id, int powerValue) {
+    node *newNode = (node *)malloc(sizeof(node));
+    if (!newNode) {
+        printf("Memory allocation failed.\n");
         return;
     }
-    while (start != NULL)
-    {
-        printf("%d->%d\n", start->id, start->power);
+    newNode->id = id;
+    newNode->power = powerValue;
+    newNode->next = NULL;
+
+    if (*start == NULL) {
+        *start = newNode;
+    } else {
+        node *traverseNode = *start;
+        while (traverseNode->next != NULL) {
+            traverseNode = traverseNode->next;
+        }
+        traverseNode->next = newNode;
+    }
+}
+
+void display(node *start) {
+    if (!start) {
+        printf("List is empty\n");
+        return;
+    }
+    while (start) {
+        printf("%d -> %d\n", start->id, start->power);
         start = start->next;
     }
 }
-int getLength(node *start)
-{
+
+int getLength(node *start) {
     int length = 0;
-    while (start != NULL)
-    {
+    while (start) {
         length++;
         start = start->next;
     }
     return length;
 }
-node *sortThePowerNonIncreasing(node *start)
-{
-    int lengthOfList = getLength(start);
-    int iterator = 0;
+
+node *sortThePowerNonIncreasing(node *start) {
     int swapped;
-    while (iterator < lengthOfList)
-    {
-        node *traverseNode = start;
-        node *previousNode = start;
+    node *currentNode, *lastSortedNode = NULL;
+
+    if (!start) return start;
+
+    do {
         swapped = 0;
-        while (traverseNode->next != NULL)
-        {
-            node *pointer = traverseNode->next;
-            if (traverseNode->power < pointer->power)
-            {
+        currentNode = start;
+        node *previousNode = NULL;
+
+        while (currentNode->next != lastSortedNode) {
+            node *nextNode = currentNode->next;
+            if (currentNode->power < nextNode->power) {
                 swapped = 1;
-                if (traverseNode == start)
-                {
-                    traverseNode->next = pointer->next;
-                    pointer->next = traverseNode;
-                    previousNode = pointer;
-                    start = previousNode;
+                if (previousNode) {
+                    previousNode->next = nextNode;
+                } else {
+                    start = nextNode;
                 }
-                else
-                {
-                    traverseNode->next = pointer->next;
-                    pointer->next = traverseNode;
-                    previousNode->next = pointer;
-                    previousNode = pointer;
-                }
-                continue;
+                currentNode->next = nextNode->next;
+                nextNode->next = currentNode;
+                previousNode = nextNode;
+            } else {
+                previousNode = currentNode;
+                currentNode = currentNode->next;
             }
-            previousNode = traverseNode;
-            traverseNode = traverseNode->next;
         }
-        if (!swapped)
-        {
-            break;
-        }
-        iterator++;
-    }
+        lastSortedNode = currentNode;
+    } while (swapped);
+
     return start;
 }
-void freeMemory(struct node **start)
-{
-    struct node *temp;
-    while (*start != NULL)
-    {
+
+void freeMemory(node **start) {
+    node *temp;
+    while (*start) {
         temp = *start;
         *start = (*start)->next;
         free(temp);
     }
-    *start = NULL;
 }
-int main()
-{
+
+int main() {
     node *start = NULL;
     int numberOfHeroes;
-    printf("\nEnter Number of Heroes:");
+
+    printf("\nEnter Number of Heroes: ");
     scanf("%d", &numberOfHeroes);
-    if (numberOfHeroes <= 0)
-    {
-        printf("Enter at least one hero.");
+
+    if (numberOfHeroes <= 0) {
+        printf("Enter at least one hero.\n");
         return 0;
     }
-    for (int iterator = 1; iterator <= numberOfHeroes; iterator++)
-    {
+
+    for (int i = 1; i <= numberOfHeroes; i++) {
         int powerValue;
-        printf("\nEnter the power level of %d:", iterator);
+        printf("\nEnter the power level of hero %d: ", i);
         scanf("%d", &powerValue);
-        createLinkedList(&start, iterator, powerValue);
+        createLinkedList(&start, i, powerValue);
     }
-    if (numberOfHeroes == 1)
-    {
+
+    printf("\nThe List of Heroes' Power Levels Before Sorting:\n");
+    display(start);
+
+    if (numberOfHeroes > 1) {
+        start = sortThePowerNonIncreasing(start);
+        printf("\nThe List of Heroes' Power Levels After Sorting:\n");
         display(start);
-        return 0;
     }
-    printf("The List of Heroes Power Level Before Sorting:\n");
-    display(start);
-    start = sortThePowerNonIncreasing(start);
-    printf("The List of Heroes Power Level After Sorting:\n");
-    display(start);
+
     freeMemory(&start);
     return 0;
 }
